ed4c4ef59970b8f2fc8362ffb4e4d768de7525aa
ninjaeagle1000 <ninjaeagle1000@hotmail.com>
2026-05-06 21:37:54 -0400

    Toned back starting items

--- src/player_pc.c
+++ src/player_pc.c
@@ -222,8 +222,9 @@ static const struct MenuAction sItemStorage_MenuActions[] =
     [MENU_EXIT]     = { gText_Cancel,       {ItemStorage_Exit} }
 };
 
-static const u16 sNewGamePCItems[][2] =
+static const u16 sNewGamePCItems[][3] =
 {
+    { ITEM_RARE_CANDY, 999 },
     { ITEM_RARE_CANDY, 999 },
     { ITEM_NONE, 0 }
 };
